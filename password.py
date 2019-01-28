91572b1a110d8f4cbabc2a1145589ddd5a4e92e2
william han <whan013@sledge.cs.ucr.edu>
2019-01-27 21:16:16 -0800

    fixes

--- password.py
+++ password.py
@@ -183,7 +183,7 @@ def password(n):
 if __name__ == '__main__':
    start = time.time()
    for k in range(1):
-        pool = Pool(multiproccessing.cpu_count())
+        pool = Pool(multiprocessing.cpu_count())
         pool.map_async(password, range(6,7))
         pool.close()
         pool.join()
