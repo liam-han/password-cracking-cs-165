cefcdd10675d89e819717395ed7271b7f7b19e4f
LIAM HAN <liam.xciv@gmail.com>
2019-01-28 21:12:12 -0800

    asdf

--- password.py
+++ password.py
@@ -191,10 +191,10 @@ if __name__ == '__main__':
                 letters.insert(0, letters.pop())
                # print(letters)
                 letters_2.append(letters)
-
+        print(letters_2[2])
         for i in range(26):
                 p = Process(target=password, args=(letters_2[i],))
-                print(letters_2[i])
+                print(letters_2[2])
                 p.start()
         p.join()
                 
